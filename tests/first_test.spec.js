67e959f22b15bfc5310b961395bc608a78f13d7f
Eduard-QA <eduard.mickel@gmail.com>
2024-04-06 13:13:22 +0300

    added file revew

--- tests/first_test.spec.js
+++ tests/first_test.spec.js
@@ -20,4 +20,22 @@ test.describe('first test', () => {
            
         })
 
-})
+}),
+
+test('has title', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+  }),
+
+  test('get started link', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+  
+    // Click the get started link.
+    await page.locator('.getStarted_Sjon').click();
+  
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+  
